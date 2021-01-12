cea3d872d59f61efd25f268a5b66da9bd1baef33
yzqdlg <1980697614@qq.com>
2021-01-12 23:01:40 +0800

    1

---
+++ test_2021_1_12/test_2021_1_12/test.c
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+#include<stdio.h>
+int fun()
+{
+	int num1 = 10, num2 = 20;
+	return (num1 + num2);
+}
+int main()
+{
+	printf("%d", fun());
+	return 0;
+}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int num1 = 10, num2 = 20;
+//	printf("%d", num1 + num2);
+//	return 0;
+//}
