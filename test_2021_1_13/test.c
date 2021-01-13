7492b0f6079fe7c2d164d2b0faf21017b794a3af
yzqdlg <1980697614@qq.com>
2021-01-13 23:21:21 +0800

    13

---
+++ test_2021_1_13/test.c
@@ -0,0 +1,27 @@
+﻿
+//#include<stdio.h>
+//int fun(a, b, c)
+//{
+//	if (a > b)
+//		c = a;
+//	else c = b;
+//	return c;
+//}
+//int main()
+//{
+//	int a, b, MAX;
+//	printf("Please enter two numbers:");
+//	scanf("%d%d", &a, &b);
+//	MAX = fun(a, b);
+//	printf("最大的数是：%d\n", MAX);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//    printf("%d\n", strlen("c:\test\121"));
+//        return 0;
+//}
