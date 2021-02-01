0405ee7275586f954774808228ea15592d9c9c1b
yzqdlg <1980697614@qq.com>
2021-02-01 23:00:23 +0800

    1

--- test_2021_2_1/test.c
+++ test_2021_2_1/test.c
@@ -32,3 +32,45 @@ int main()
 	}
 	return 0;
 }
+//void bubble_sort(int arr[],int sz)
+//{
+//	int i = 0;
+//	int len = sz - 1;
+//	while(len>1)
+//	{
+//		int k = 1;
+//		int tmp = 0;
+//		for (i = 0; i < len - 1; i++)
+//		{
+//			if (arr[i] > arr[i + 1])
+//			{
+//				tmp = arr[i + 1];
+//				arr[i + 1] = arr[i];
+//				arr[i] = tmp;
+//				k = 0;//ÅĞ¶ÏÊÇ·ñÅÅĞòÍê³É
+//			}
+//		}
+//		len = len - 1;
+//		if (k)
+//		{
+//			break;
+//		}
+//	} 
+//}
+//void print_arr(int arr[],int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	int arr[] = { 5,6,3,2,8,9,4,1,7,10,11,13,12 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr,sz);
+//	print_arr(arr,sz);
+//	return 0;
+//}
